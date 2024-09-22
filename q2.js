88df4b8843c241d57c7c7f97b320088c5631968e
CalebScott0 <calebbenscott@gmail.com>
2024-09-22 00:04:38 -0500

    workshop q 1 & 2

--- q2.js
+++ q2.js
@@ -1,7 +1,13 @@
 // each person may only buy one ticket a time and then must go to the back of the qeue
 
+// ** find a way to do this that does not empty the queue fully before next iteration **
+  // maybe with a map of index in line at the beginning and # of tickets
+    // and a queue to iterate through line?
+    // let queue = [...arr];
+    // let queue = Array(...arr);
+    // let queue = Array.from([...arr]);
+
 const ticketTime = (arr, k) => {
-  // let queue = [...arr];
   let queue = [];
   let count = 0;
   // perform queue operations until target index has bought all tickets
@@ -16,6 +22,7 @@ const ticketTime = (arr, k) => {
         //   exit if target is at 0 to avoid going all through way back through array
         if (arr[k] === 0) {
           while (queue.length > 0) {
+            // target k will be at end of queue, return count after queue is empty
             queue.shift();
             count++;
           }
@@ -23,6 +30,7 @@ const ticketTime = (arr, k) => {
         }
       }
     }
+    // add 1 to count for each in line if target is not on their last ticket
     while (queue.length > 0) {
       queue.shift();
       //  add to count of time for each ticket sold if target is not at 0 in iteration
--- q3.js
+++ q3.js
@@ -1,1 +1,2 @@
 // COUNT INVERSIONS IN SORTS (google inversions to understand fully)
+// Use selection sort ?
