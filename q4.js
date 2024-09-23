d7f966f74ea8463bd55cc09d9e0dae7818ea5853
CalebScott0 <calebbenscott@gmail.com>
2024-09-23 16:42:30 -0500

    workshop q3

--- q4.js
+++ q4.js
@@ -24,29 +24,51 @@ const arr = [
 // const sortedArr2 = arr.sort((a, b) => (a[0] < b[0] ? 1 : -1));
 // 2 sorts above produces below array that can be sorted into final answer
 // [ [ 9, 0 ], [ 8, 0 ], [ 6, 2 ], [ 5, 0 ], [ 5, 2 ], [ 5, 5 ] ]
+// sort without sort function =  [array[j - 1], array[j]] = [array[j], array[j - 1]] to swap items in nested array
 // -> second idx sorted in decreasing order, and then first idx sorted in decreasing order
 /* -> this can be broken apart into sorting [0] if first idx of compare element doesn't match
  *      or sorting [1] if first idxs do match
  *      done as a modified insertion sort?
  */
+
 const sort = (array) => {
+  console.log(array);
+  // function to swap items in array
+  const swap = (arr, x, y) => {
+    let temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+  };
   // insertion sort to move elements into correct position based on value and second idx
   for (let i = 1; i < array.length; i++) {
-    // second idx is the amount of items[0] before it that need to be greater or equal
-    // let timesToMove = array[i][1];
-    // move item at current idx until it has passed x elements >= current
+    // j to represent sorted boundary to check against decrementally for insertion sort
+    // sortCheck will terminate while loop if either first index of predecessor is greater than or equal to first idx of j
+    // or if second index of predecessor is less than or equal to second idx of j
     let j = i;
-    while (j > 0) {
-      // if (array[j + 1][0] >= array[j][0]) {
-      //   if (array[j + 1][0] >= array[j][0] && j < array.length - 1) {
-      // timesToMove--;
-      let temp = array[j + 1];
-      array[j + 1] = array[j];
-      array[j] = temp;
-      j--;
+    let sortCheck = true;
+    while (j > 0 && sortCheck) {
+      // check if first item in inner array item equals predecessor to determine sort type
+      // if second index item of predecessor is greater than j, swap
+      if (array[j - 1][0] === array[j][0] && array[j - 1][1] > array[j][1]) {
+        swap(array, j - 1, j);
+        j--;
+        // if first indexes are not equal, sort based on first index, greater values towards the front
+      } else if (
+        array[j - 1][0] !== array[j][0] &&
+        array[j - 1][0] < array[j][0]
+      ) {
+        swap(array, j - 1, j);
+        j--;
+      } else {
+        sortCheck = false;
+      }
     }
   }
-  // }
+  const newQueue = [];
+  array.forEach((item) => {
+    let idx = item[1];
+    console.log(idx);
+  });
   return array;
 };
-console.log(sort(sortedArr2));
+console.log(sort(arr));
